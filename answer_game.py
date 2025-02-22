5e9ead10c8fdcd1552ca24ad7f268ba411031ad6
Sócrates Batista Antonopoulos <antonopoulosdev@outlook.com>
2025-02-22 12:40:45 -0300

    adicionado sistema de cadastro de jogadores e .gitignore

--- answer_game.py
+++ answer_game.py
@@ -3,20 +3,19 @@ from random import shuffle
 import time
 from tkinter import *
 from tkinter import ttk
-
-
+from name_insert import name_window
 
 root = Tk()
 root.title("Hello World!")
 root.geometry("700x500")
 
-def iniciar_jogo():
 
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Sejam bem vindos ao nosso primeiro teste!").grid(column=0, row=0, columnspan=2)
-    ttk.Button(frm, text="Iniciar", command="").grid(column=0, row=2)
-    ttk.Button(frm, text="Sair", command=root.destroy).grid(column=1, row=2)
+
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Sejam bem vindos ao nosso primeiro teste!").grid(column=0, row=0, columnspan=2)
+ttk.Button(frm, text="Iniciar", command=lambda:name_window(root)).grid(column=0, row=2)
+ttk.Button(frm, text="Sair", command=root.destroy).grid(column=1, row=2)
 
 
 root.mainloop()
---
+++ name_insert.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from tkinter import ttk, messagebox, Toplevel
+import json
+import os
+
+def pegar_nome(name , entrada):   
+    nome = entrada.get()
+    if os.path.exists("jogadores.json"):
+        with open("jogadores.json", "r") as arquivo:
+            try:
+                jogadores = json.load(arquivo)
+            except json.JSONDecodeError:
+                jogadores = []
+    else:
+        jogadores = []
+
+    if any (jogador["nome"] == nome for jogador in jogadores):
+        messagebox.showwarning("Atenção!","Já existe um jogaodr com este nome!")
+    else:
+        novo_jogador = {"nome": nome, "pontos": 0}
+        jogadores.append(novo_jogador)
+
+        with open("jogadores.json", "w") as arquivo:
+            json.dump(jogadores, arquivo)
+        print(f"Nome salvo: {nome}")
+        name.destroy()
+
+def name_window(root):
+
+    name = Toplevel(root)
+    name.title("Insira seu nome!")
+    name.geometry("300x130")
+    name.resizable(False, False)
+
+    texto = ttk.Label(name, text="Insira seu nome:")
+    texto.pack(pady=5)
+
+    entrada = ttk.Entry(name)
+    entrada.pack(pady=5) 
+
+    botao = ttk.Button(name, text="Inserir nome", command=lambda:pegar_nome(name, entrada))
+    botao.pack(pady=10)
+
+
+        
+
+    name.mainloop()
